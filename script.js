2a45fcc095ec7c7436faffebe90b41dba2181c73
Anita RASOA <anita.ras@onja.org>
2020-07-04 14:26:45 +0300

    Creating a new blog

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 console.log('it works');
 
-// code your solution in here
+const newPost = () => {
+        var div = document.createElement('div');
+        div.setAttribute('id', 'post-list');
+
+        var title = document.createElement('h5');
+        title.classList.add('card-title');
+
+        var content = document.createElement('p');
+        content.classList.add('card-text');
+
+        title.textContent = document.getElementById('new-post-title').value;
+        content.textContent = document.getElementById('new-post-content').value;
+
+        div.appendChild(title);
+        div.appendChild(content);
+        return div;
+    }
+
+const blogList = document.querySelector('#post-list');
+
+const submit = document.getElementById('submit-form');
+
+submit.addEventListener('click', ($event) => {
+    const newBlog = newPost();
+    blogList.appendChild(newBlog);
+    $event.preventDefault();
+    document.querySelector('form').reset();
+  });
+
